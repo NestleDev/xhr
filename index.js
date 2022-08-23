62bf20c379daa9548d63eed1dc6783bb5ba50a2a
Alex Nesvit <a.nesvit@dataduck.com>
2022-08-23 21:00:53 +0300

    sdsd

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 let headers = new Headers();
 
 headers.append('Content-Type', 'application/json');
+headers.append('Accept', 'application/json');
+
+headers.append('Access-Control-Allow-Origin', 'https://nestledev.github.io');
+
+headers.append('GET', 'POST', 'OPTIONS');
 fetch('https://webdev-api.loftschool.com/sendmail', {
     method: "POST",
     mode: 'no-cors',
