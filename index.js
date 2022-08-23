6558c0cce2dbd92c9f83ad7083056732e8de5230
Alex Nesvit <a.nesvit@dataduck.com>
2022-08-23 20:57:19 +0300

    init

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let headers = new Headers();
 headers.append('Content-Type', 'application/json');
 fetch('https://webdev-api.loftschool.com/sendmail', {
     method: "POST",
+    mode: 'no-cors',
     headers: headers,
     body: JSON.stringify({
         name: 'alex',
