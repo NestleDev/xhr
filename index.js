7388bc2f74b258f4f3f7f89a9ce0f824adce862b
Alex Nesvit <a.nesvit@dataduck.com>
2022-08-23 21:03:48 +0300

    sdsd

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ headers.append('Access-Control-Allow-Origin', 'https://nestledev.github.io');
 headers.append('GET', 'POST', 'OPTIONS');
 fetch('https://webdev-api.loftschool.com/sendmail', {
     method: "POST",
-    mode: 'no-cors',
     headers: headers,
     body: JSON.stringify({
         name: 'alex',
